bf208dbc6ddcb1692c7b188a1124b2bf224e57c2
Shaine Buzzard <s.buzzard7@outlook.com>
2025-01-12 19:43:16 -0500

    updated

--- src/Weather.js
+++ src/Weather.js
@@ -40,6 +40,7 @@ const Weather = () => {
           value={city}
           onChange={handleInputChange}
         />
+        <button type="submit">Get Weather</button>
       </form>
       {weatherData ? (
         <>
